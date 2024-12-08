510f0cc23022b385c36c24aa1086c204b5caeaf4
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2024-12-08 13:03:20 +0400

    დავალება წაიშალა და თავიდან ავტვირთე

--- task5.py
+++ task5.py
@@ -1,8 +1,18 @@
 #შევქმენით ფუნქცია caesar_cipher
-def caesar_cipher(st,num):
-    #შევემქნით ცვლადი str რომელიც არის ცარიელი სტრინგი ,ეს ცვლადი
-    str = ""
-    
+def caesar_hiper(text, num):
+    # შევქმენით recult ცვლაი რომელიც არის ცარიელი სტრინგი სადაც შეინახება საბოლოო შედეგი
+    result = ''
+    for i in text:
+        #ვამოწმებთ ანუ არის თუ არა ჩვენი ელემენტი ანბანის რმელიმე ასო
+        if i.isalpha():
+            #ვადგენთ დიდი ასოა თუ პატარა
+            bool = 'A' if i.isupper() else 'a'
+            result += chr((ord(i) - ord(bool) + num)% 26 + ord(bool))
+        else:
+            #თუ სიმბოლო არ არის ასო მაშინ პირდაპირ ვამატებთ შედეგს
+            result += i
+        #ბოლოს კი ვაბრუნებთ ჩვენს შედეგს
+    return result
 
 
         
